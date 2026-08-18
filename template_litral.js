d7f69d2b1408a42c5ccba19960dafa9315e39acb
Shrija Lal <shrijalal6@gmail.com>
2026-08-18 11:17:40 +0530

    basics practice

---
+++ template_litral.js
@@ -0,0 +1,6 @@
+obj={
+    name:"abc",
+    price:782,
+}
+console.log(`the cost of ${obj.name} is ${obj.price}Rs`);  // template literal
+console.log(`The sum of 1 and 3 is ${1+3}`);  // template literal
