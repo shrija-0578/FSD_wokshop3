aacaee05655e227175b1fdb14564354a31e6834c
Shrija Lal <shrijalal6@gmail.com>
2026-09-15 11:02:43 +0530

    practice question experiment 2

---
+++ experiment_2/assingment_2.js
@@ -0,0 +1,37 @@
+const fs = require("fs"); 
+fs.writeFile("student.txt","Name : john \nRoll No : 123\nBranch : CSE\nsemester : 3",(err)=>{
+    if (err) throw err
+    else
+    {
+        console.log("File successfully created")
+    }
+});
+// read
+fs.readFile("student.txt","utf-8",(err,data)=>{
+    if (err) throw err
+    else
+    {
+        console.log("File content is: ");
+        console.log(data);
+    }
+});
+//update
+fs.appendFile("student.txt","\nSubject : FSD\nmarks : 95\nAttendence : 90",(err)=>{
+    if (err) throw err
+    else
+    {
+        console.log("File updated successfully");
+    }
+});
+
+// read again
+fs.readFile("student.txt","utf-8",(err,data)=>{
+    if (err) throw err
+    else
+    {
+        console.log("File content is: ");
+        console.log(data);
+    }
+});
+// 
+console.log("successfully done");
